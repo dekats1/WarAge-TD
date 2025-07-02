7774bc8e45c7fb4f5cb8e7626234139d77fc4e15
dekats <denislatcko@gmail.com>
2025-07-02 20:46:44 +0300

    Initial commit

---
+++ src/main/java/com/warage/StartGame.java
@@ -0,0 +1,12 @@
+package com.warage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StartGame extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
